8595f1dd43417ad67e60cce2be35b3cad7e24f4a
pkxxx <pkxxx@spoko.pl>
2025-11-01 15:47:16 +0100

    task manager v1

---
+++ TaskManager.py
@@ -0,0 +1,52 @@
+class Task:
+    def __init__(self, task_name, due_date):
+        self.task_name = task_name
+        self.due_date = due_date
+
+class TaskManager(Task):
+    def __init__(self):
+        self.tasks = []
+
+    def add_task(self, task_name, due_date):
+        task = Task(task_name, due_date)
+        self.tasks.append(task)
+
+    def remove_task(self, task_name):
+        task_exists = False
+        for task in self.tasks:
+            if task.task_name == task_name:
+                self.tasks.remove(task)
+                task_exists = True
+                break
+
+        if not task_exists:
+            print("Nie można odnaleźć zadania o podanejnazwie: ", task_name)
+
+    def display_tasks(self):
+        if not self.tasks:
+            print("Brak zadań do wyświetlenia.")
+        else:
+            for task in self.tasks:
+                print(task.task_name, "| Termin:", task.due_date)
+
+    def change_due_date(self, task_name, new_due_date):
+        task_exists = False
+        for task in self.tasks:
+            if task.task_name == task_name:
+                task.due_date = new_due_date
+                task_exists = True
+                print("Termin zadania", task_name, "został zmieniony na:", new_due_date)
+                break
+        if not task_exists:
+            print("Nie można odnaleźć zadania o podanej nazwie:", task_name)
+
+task_manager = TaskManager()
+task_manager.add_task("Umyj naczynia", "01.12.2023")
+task_manager.add_task("wynieś śmieci", "07.12.2023")
+task_manager.display_tasks()
+print("--------------")
+task_manager.change_due_date("Umyj naczynia", "31.12.2023")
+task_manager.display_tasks()
+print("--------------")
+task_manager.remove_task("wynieś śmieci")
+task_manager.display_tasks()
